539761af193530c9f207450e8e707c325ef975ea
Carlos Augusto Faustino Tomazin <65822302+carlostomazin@users.noreply.github.com>
2021-04-25 20:00:25 -0300

    Add files via upload

--- Codigos_Aula/Aula 5/A05_Bônus03 - ODS com função Hash.c
+++ Codigos_Aula/Aula 5/A05_Bônus03 - ODS com função Hash.c
@@ -4,36 +4,15 @@
 #define M 97
 int colisao = 0;
 
-typedef struct{
+typedef struct _estado{
     int chave_hash;
     char estado[2];
     int calc_ascii;
 }ods;
 
-typedef struct{
-    char estados[2];
+typedef struct _tabelaHash{
+    char estado[2];
 }valor;
-
-// void tabelaHash(ods *dad, valor *vet){
-//     int ctd,i;
-
-//     for(ctd=0; ctd<num; ctd++){
-//         if( vet[dad[ctd].chave_hash + i].estados == " "){
-//             strcpy(vet[dad[ctd].chave_hash + i].estados,dad[ctd].estado);
-//         }else{
-//             i = dad[ctd].chave_hash + 1;
-//             colisao = 1;
-//             while (1){
-//                 if( vet[i].estados == " "){
-//                     strcpy(vet[i].estados,dad[ctd].estado);
-//                     break;
-//                 }
-//                 colisao++;
-//                 i++;
-//             }
-//         } 
-//     }
-// }
 
 int main (){
     ods dados[num];
@@ -47,49 +26,44 @@ int main (){
     }
 
     int ctd,i, hash;
-    
-    for(ctd=0;ctd<num;ctd++){
+
+    /* for para zerar a varivel calc_ascci de todos os estados. */
+    for(ctd=0; ctd<num; ctd++){
         dados[ctd].calc_ascii = 0;
     }
-    
+
+    /* for para ler as entradas apartir de um arquivo. */
     for(ctd=0;ctd<num;ctd++){
         fscanf(arqEnt,"%s", dados[ctd].estado);
-        
+        /* for para gerar a chave hash apartir do valor ascii da string. */
         for(i = 0; i < strlen(dados[ctd].estado); i++){
             dados[ctd].calc_ascii += (int)dados[ctd].estado[i];
             dados[ctd].chave_hash = dados[ctd].calc_ascii % num;
             
         }
     }
-
+    /* for para passar por todos os estados e ir alocando eles na tabela Hash com sua determinada posição. */
     for(ctd=0; ctd<num; ctd++){
-        if( strlen(vetor[dados[ctd].chave_hash].estados) <= 0){
-            strcpy(vetor[dados[ctd].chave_hash].estados, dados[ctd].estado);
-            printf("certo\n");
+        /* if para verificar se o espaço de memoria está vazio, se sim alocar o conteudo da var estado 
+        na posição(usando a chave hash para essa posição) do vetor tabelaHas, ... */
+        if( strlen(vetor[dados[ctd].chave_hash].estado) <= 0){
+            strcpy(vetor[dados[ctd].chave_hash].estado, dados[ctd].estado);
         }else{
+            /* se não vai se criar um laço para ir pulando de posição em posição, até achar um espaço vazio. */
             i = dados[ctd].chave_hash + 1;
-            
+            colisao++;
             while (1){
-                if( strlen(vetor[i].estados) <= 0){
-                    strcpy(vetor[i].estados, dados[ctd].estado);
-                    printf("certo\n");
-                    colisao++;
+                if( strlen(vetor[i].estado) <= 0 && i < M){
+                    strcpy(vetor[i].estado, dados[ctd].estado);
                     break;
                 }
-                
+                colisao++;
                 i++;
             }
         } 
     }
 
-    //tabelaHash(dados,vetor);
-
-    // for(ctd=0;ctd<num;ctd++){
-    //     //printf("ASCII: %d   ", dados[ctd].calc_ascii);
-    //     printf("%d\n",dados[ctd].chave_hash);
-    // }
-
-    printf("%d",colisao);
+    printf("Numero de colisoes: %d",colisao);
 
     fclose(arqEnt);
     return 0;
