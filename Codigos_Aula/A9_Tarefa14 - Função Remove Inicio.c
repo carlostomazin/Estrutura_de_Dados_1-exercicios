0f95aceb99dd6deebc32d1c11546e7fac76318ac
carlostomazin <carlos.tomazin2@gmail.com>
2021-05-22 14:44:14 -0300

    func

---
+++ Codigos_Aula/A9_Tarefa14 - Função Remove Inicio.c
@@ -0,0 +1,20 @@
+//---Essa função realiza a tarefa de remover o primiero elemento da lista---//
+
+void removeInicio(No **Lista){
+
+    /*Uma variavei do tipo No para trabalhar com a Lista.(Sendo '*Lista' o inicio da lista).*/
+    No *atual = *Lista;
+
+    /*Ver se a lista está vazia, se estiver encerra a função.*/
+    if(listaEhVazia(*Lista))
+		return;
+    
+	printf("Removendo %d\n",atual->dado);
+    /*Passando o INICIO para o segundo elemento da lista.(Usando o apontamento do primeiro elemento)*/
+    *Lista = atual->proximo;
+    /*Removendo o primeiro elemento da lista.*/
+    atual = NULL;
+
+    /*Liberando a memória do primeiro elemento.*/
+    free(atual);
+}
